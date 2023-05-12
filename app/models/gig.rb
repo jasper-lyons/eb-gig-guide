db3db901d38af3bd93997e5d962d6ad8f0a3a58e
Jasper Lyons <jasper.lyons@gmail.com>
2023-05-12 10:35:55 +0100

    fix sqlite / postgres difference

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -2,4 +2,13 @@ class Gig < ApplicationRecord
   def doors_time_only
     doors&.strftime('%l:%M%P')
   end
+
+  def date
+    # This is to faciliate the different between dev and prod
+    # in dev we use sqlite which stores dates as strings, in prod
+    # we use postgres which stores dates as datetimes
+    return Date.parse(self[:date]) if self[:date].is_a?(String)
+
+    self[:date]
+  end
 end
