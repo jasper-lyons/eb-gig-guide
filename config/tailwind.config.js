d1146d899150b6cdac4f81fdb0616ba609499515
Jasper Lyons <jasper.lyons@gmail.com>
2023-11-20 17:39:45 +0000

    feat: add chalk board and new font

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter Font',...defaultTheme.fontFamily.sans],
-        display: ['Bebas Neue']
+        display: ['Cabin Sketch']
       },
     },
   },
