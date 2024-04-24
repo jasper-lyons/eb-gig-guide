49f79d1d715b0b4a6972ebe92a934dfc0a69e039
Jasper Lyons <jasper.lyons@gmail.com>
2024-04-24 23:41:24 +0100

    correct table name

--- db/migrate/20240424161245_create_acts_gigs.rb
+++ db/migrate/20240424161245_create_acts_gigs.rb
@@ -1,6 +1,6 @@
 class CreateActsGigs < ActiveRecord::Migration[7.0]
   def change
-    create_table :acts_gig do |t|
+    create_table :acts_gigs do |t|
       t.references :act, null: true, foreign_key: true
       t.references :gig, null: true, foreign_key: true
 
